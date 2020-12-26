0a5925b64f24b0d6fe4b87272579175f7abebf55
Russell Geraghty <russell@rosesareblue.co.uk>
2020-12-26 16:29:19 +0000

    Broke conversations into direct and public channels

--- features/slack_features.js
+++ features/slack_features.js
@@ -5,7 +5,21 @@
 const path = require('path');
 const { NlpManager } = require('node-nlp');
 
-let nlpjs = null;
+let privateNlp = null;
+let publicNlp = null;
+
+/**
+ * Build the NLP.
+ *
+ * @param settings The settings
+ * @param filename The filename
+ * @returns {Promise<NlpManager>}
+ */
+const buildNlp = async (settings, filename) => {
+  const nlp = new NlpManager(settings);
+  await nlp.load(path.join(__dirname, filename));
+  return nlp;
+};
 
 const nlpReady = new Promise(async resolve => {
   const settings = {
@@ -16,26 +30,32 @@ const nlpReady = new Promise(async resolve => {
   if (process.env.DUCKLING_URL) {
     settings.ner = { ducklingUrl: process.env.DUCKLING_URL };
   }
-
-  const nlp = new NlpManager(settings);
-
-  await nlp.load(path.join(__dirname, 'model.json'));
-  nlpjs = nlp;
+  privateNlp = await buildNlp(settings, 'private.json');
+  publicNlp = await buildNlp(settings, 'public.json');
   resolve();
 });
 
-async function handleMessageAndReplyThread(message, bot) {
-  const reply = await nlpjs.process(message.text);
-  console.log(JSON.stringify(reply, null, 2));
-  if (reply.intent !== 'None' && reply.answer) {
-    await bot.replyInThread(message, reply.answer);
+/**
+ * Handle a public message.
+ *
+ * @param message The message to process
+ * @param bot The bot to reply to
+ * @returns {Promise<void>}
+ */
+const handlePublicMessageAndReplyThread = async (message, bot) => {
+  if (message.text) {
+    const reply = await publicNlp.process(message.text);
+    if (reply.intent !== 'None' && reply.answer) {
+      await bot.replyInThread(message, reply.answer);
+    }
+  } else {
+    await bot.replyInThread(message, 'You rang?');
   }
-}
+};
 
-module.exports = function (controller) {
+module.exports = controller => {
   controller.ready(async () => {
     await nlpReady;
-
     console.log('NLP ready to go');
 
     if (process.env.MYTEAM) {
@@ -46,7 +66,7 @@ module.exports = function (controller) {
   });
 
   controller.on('direct_message', async (bot, message) => {
-    const reply = await nlpjs.process(message.text);
+    const reply = await privateNlp.process(message.text);
     console.log(JSON.stringify(reply, null, 2));
 
     if (reply.intent === 'None') {
@@ -57,10 +77,17 @@ module.exports = function (controller) {
   });
 
   controller.on('direct_mention', async (bot, message) => {
-    await handleMessageAndReplyThread(message, bot);
+    await handlePublicMessageAndReplyThread(message, bot);
   });
 
   controller.on('mention', async (bot, message) => {
-    await handleMessageAndReplyThread(message, bot);
+    await handlePublicMessageAndReplyThread(message, bot);
+  });
+
+  /**
+   * Listen to everything and see if we can help.
+   */
+  controller.hears('.*', 'message', async (bot, message) => {
+    await handlePublicMessageAndReplyThread(message, bot);
   });
 };
--- train/index.js
+++ train/index.js
@@ -1,10 +1,35 @@
 const path = require('path');
 const NlpManager = require('node-nlp/src/nlp/nlp-manager');
 
-(async () => {
+const privateTraining = [
+  'small-talk/agent.json',
+  'small-talk/appraisal.json',
+  'small-talk/greetings.json',
+  'small-talk/user.json',
+];
+
+const publicTraining = ['conversation-starters/starters.json', 'small-talk/greetings.json'];
+
+const toPath = paths => {
+  return paths.map(p => path.join(__dirname, p));
+};
+
+/**
+ * Create the training model of the given name.
+ *
+ * @param name The name to save
+ * @param corpora The corpora to train on
+ * @returns {Promise<void>}
+ */
+const train = async (name, corpora) => {
   const nlp = new NlpManager({ languages: ['en'], forceNER: true, trainByDomain: true });
-  await nlp.addCorpus(path.join(__dirname, './corpus-en.json'));
+  await nlp.addCorpora(corpora);
 
   await nlp.train();
-  nlp.save(path.join(__dirname, '..', 'features', 'model.json'));
+  nlp.save(path.join(__dirname, '..', 'features', `${name}.json`));
+};
+
+(async () => {
+  await train('private', [...toPath(privateTraining), ...toPath(publicTraining)]);
+  await train('public', [...toPath(publicTraining)]);
 })();
